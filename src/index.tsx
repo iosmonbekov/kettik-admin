094579f8942383acada683d83ce655469981557d
iosmonbekov <iosmonbekov666@gmail.com>
2023-01-11 22:17:59 +0600

    Check release

--- src/index.tsx
+++ src/index.tsx
@@ -5,5 +5,5 @@ import ReactDOM from 'react-dom/client';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render(<h1>Kotok</h1>);
+root.render(<h1>Am Bash</h1>);
 
